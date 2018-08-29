116fcd7d79a2602397b4d798b1e1d11e6e60625f
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-08-29 17:13:23 -0300

    Added basic plural detection support for Word

--- esperanto_analyzer/words/base.py
+++ esperanto_analyzer/words/base.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 class Word:
 
-  def __init__(self, content):
-    self.__validate_content(content)
+  # Only words with at least 4 characteres can be in plural, so this exclude
+  # words such "ajn" and "kaj".
+  PLURAL_DETECT_REGEXP = re.compile('.{2,}([^n]j|jn)$', re.IGNORECASE|re.UNICODE)
+
+  def __init__(self, content, context = None):
+    self._validate_content(content)
 
     self.content = content
+    self.context = context
     self.metadata = dict()
+    self.plural = (self._match_plural(context) not in [False, None])
 
-  def __validate_content(self, content):
+  def has_plural(self):
+    return True
+
+  def _match_plural(self, context = None):
+    if not self.has_plural(): return None
+
+    return self.PLURAL_DETECT_REGEXP.match(self.content)
+
+  def _validate_content(self, content):
     if not content:
       raise NotContentError
 
--- tests/words/test_base.py
+++ tests/words/test_base.py
@@ -28,3 +28,40 @@ class TestWordBasic():
     word = Word(' ')
 
     assert(word.metadata == dict())
+
+
+class TestValidWordPlural():
+  def test_esperanto_words(self):
+    for word in ['kaj', 'ajn']:
+      assert(Word(word).plural == False)
+
+  def test_plural_without_acusative(self):
+    word = Word('domoj')
+
+    assert(word.plural == True)
+
+  def test_plural_with_acusative(self):
+    word = Word('domojn')
+
+    assert(word.plural == True)
+
+class TestInvalidWordPlural():
+  def test_plural_without_acusative(self):
+    word = Word('domo')
+
+    assert(word.plural == False)
+
+  def test_plural_with_acusative(self):
+    word = Word('domon')
+
+    assert(word.plural == False)
+
+class TestPluralWord(Word):
+  def has_plural(self):
+    return False
+
+class TestValidWordDontHasPlural:
+  def test_valid_word_plural(self):
+    word = TestPluralWord('multe')
+
+    assert(word.plural == False)
