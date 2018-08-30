333915e42d32d2f92001e89586b01ce0f2ac8fe6
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-08-30 16:11:28 -0300

    Added execution cache mechanism to BaseMorphologicalAnalyzer to avoid multiples call to '#analyze()' method

--- esperanto_analyzer/analyzers/morphological/base.py
+++ esperanto_analyzer/analyzers/morphological/base.py
@@ -13,20 +13,28 @@ class BaseMorphologicalAnalyzer:
         self.raw_word = raw_word
         self.word = None
         self.matches = dict()
+        self.processed = False
+
+    def match(self):
+        return self.MATCH_REGEXP.match(self.raw_word)
 
     def analyze(self):
+        if self.processed is True:
+            return None
+
         matches = self.match()
 
+        # Set as the first time runned
+        self.processed = True
+
         if matches:
-            self.matches = matches
             self.word = self.word_class()(self.raw_word)
+            self.matches = matches
 
             return True
 
         return False
 
-    def match(self):
-        return self.MATCH_REGEXP.match(self.raw_word)
-
-    def word_class(self):
+    @staticmethod
+    def word_class():
         raise NotImplementedError
