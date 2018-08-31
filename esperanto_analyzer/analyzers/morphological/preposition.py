b98f92e71b20bcb36d2bf56c8cdf6c0a1849e78e
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-08-31 16:26:56 -0300

    Added 'PrepositionMorphologicalAnalyzer' capability to detect custom Esperanto's prepositions and added basics tests for it

--- esperanto_analyzer/analyzers/morphological/preposition.py
+++ esperanto_analyzer/analyzers/morphological/preposition.py
@@ -5,8 +5,21 @@ from esperanto_analyzer.speech import Preposition
 from esperanto_analyzer.analyzers.morphological import BaseMorphologicalAnalyzer
 
 class PrepositionMorphologicalAnalyzer(BaseMorphologicalAnalyzer):
-    #  MATCHES: ["patro", "patroj", "patron", "patrojn"]
-    MATCH_REGEXP = re.compile('^(.)$', re.IGNORECASE|re.UNICODE)
+    PREPOSITIONS_LIST = ['K', 'al', 'anstataŭ', 'antaŭ', 'antaŭ ol', 'apud', 'da', 'de', 'disde',
+                         'du vortoj', 'dum', 'ekde', 'ekster', 'eksteren', 'el', 'en', 'ene',
+                         'estiel', 'far', 'fare de', 'flanke de', 'for de', 'graŭ', 'inter', 'je',
+                         'kaj ankaŭ', 'kiel', 'kontraŭ', 'kontraŭe de', 'krom', 'kun', 'laŭ',
+                         'mala', 'malantaŭ', 'malgraŭ', 'malkiel', 'malsupre de', 'malsupren',
+                         'meze de', 'na', 'nome de', 'ol', 'per', 'pere de', 'plus', 'po', 'por',
+                         'post', 'preter', 'pri', 'pro', 'proksime de', 'samkiel', 'sed', 'sekva',
+                         'sen', 'sub', 'suben', 'super', 'supren', 'sur', 'tiu', 'tiuj', 'tra',
+                         'trans', 'tri vortoj', 'tuj post', 'tutĉirkaŭ',
+                         'ĉe', 'ĉi tiu', 'ĉi tiuj', 'ĉirkaŭ', 'ĝis']
+
+    PROPOSITIONS_MATCH_REGEXP = re.compile('|'.join(PREPOSITIONS_LIST), re.IGNORECASE|re.UNICODE)
+
+    #  MATCHES only elements in `PREPOSITIONS_LIST`
+    MATCH_REGEXP = re.compile('^(%s)$' % (PROPOSITIONS_MATCH_REGEXP.pattern), re.IGNORECASE|re.UNICODE)
 
     @staticmethod
     def word_class():
