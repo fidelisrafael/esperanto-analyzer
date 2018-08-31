a2975d600edcbd938333cc583218dd6569f75d0e
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-08-31 13:24:46 -0300

    Creating ArticleMorphologicalAnalyzer.MATCH_REGEXP through ARTICLE_LIST = ["la"] list.

--- esperanto_analyzer/analyzers/morphological/article.py
+++ esperanto_analyzer/analyzers/morphological/article.py
@@ -5,7 +5,12 @@ from esperanto_analyzer.speech import Article
 from esperanto_analyzer.analyzers.morphological import BaseMorphologicalAnalyzer
 
 class ArticleMorphologicalAnalyzer(BaseMorphologicalAnalyzer):
-    MATCH_REGEXP = re.compile('^(la)$', re.IGNORECASE|re.UNICODE) #  MATCHES: ["la"]
+    ARTICLES_LIST = ['la']
+
+    ARTICLES_MATCH_REGEXP = re.compile('|'.join(ARTICLES_LIST), re.IGNORECASE|re.UNICODE)
+
+    #  MATCHES: ["la"]
+    MATCH_REGEXP = re.compile('^(%s)$' % (ARTICLES_MATCH_REGEXP.pattern), re.IGNORECASE|re.UNICODE)
 
     @staticmethod
     def word_class():
