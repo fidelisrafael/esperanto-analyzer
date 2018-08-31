08a86dce3183814e5f865660f3877b91b1ded6f4
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-08-31 13:27:20 -0300

    Added 'ConjunctionMorphologicalAnalyzer' capability to detect custom Esperanto conjunctions and added basics tests for it

--- esperanto_analyzer/analyzers/morphological/conjunction.py
+++ esperanto_analyzer/analyzers/morphological/conjunction.py
@@ -5,8 +5,39 @@ from esperanto_analyzer.speech import Conjunction
 from esperanto_analyzer.analyzers.morphological import BaseMorphologicalAnalyzer
 
 class ConjunctionMorphologicalAnalyzer(BaseMorphologicalAnalyzer):
-    #  MATCHES: ["patro", "patroj", "patron", "patrojn"]
-    MATCH_REGEXP = re.compile('^(.)$', re.IGNORECASE|re.UNICODE)
+    CONJUNCTIONS_LIST = [
+        'antaŭ kiam',
+        'antaŭ ol'
+        'au',
+        'aŭ',
+        'ĉar',
+        'ĉu',
+        'K',
+        'k',
+        'kaj',
+        'kaŭ',
+        'ke',
+        'kial',
+        'kiam',
+        'kie',
+        'kiel',
+        'kune kun',
+        'kvankam',
+        'kvazau',
+        'kvazaŭ',
+        'minus',
+        'nek',
+        'ol',
+        'plus',
+        'se',
+        'sed',
+        'tial'
+    ]
+
+    CONJUCTIONS_MATCH_REGEXP = re.compile('|'.join(CONJUNCTIONS_LIST), re.IGNORECASE|re.UNICODE)
+
+    #  MATCHES only elements in `CONJUNCTIONS_LIST`
+    MATCH_REGEXP = re.compile('^(%s)$' % (CONJUCTIONS_MATCH_REGEXP.pattern), re.IGNORECASE|re.UNICODE)
 
     @staticmethod
     def word_class():
