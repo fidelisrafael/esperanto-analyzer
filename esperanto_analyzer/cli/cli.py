1dfb492474f916c1a3b2ae98118a61f2b3e8c7a0
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-09-01 14:01:17 -0300

    Added tests to CLI module ;)

--- esperanto_analyzer/cli/cli.py
+++ esperanto_analyzer/cli/cli.py
@@ -1,7 +1,11 @@
 # pylint: disable=missing-docstring
+import sys
 import tableprint
 
 from esperanto_analyzer import MorphologicalSentenceAnalyzer
+
+class AnalyzerNotProcessedError(BaseException):
+    pass
 
 class CLI():
     COLORS = {
@@ -20,21 +24,21 @@ class CLI():
     OUTPUT_TABLE_HEADERS = ['Word', 'Part of Speech']
 
     @staticmethod
-    def run(input_sentence=None):
+    def run(input_sentence=None, output=sys.stdout):
         analyzer = MorphologicalSentenceAnalyzer(input_sentence)
         analyzer.analyze()
 
-        CLI.display_output_for_analyzer(analyzer)
+        CLI.display_output_for_analyzer(analyzer, output=output)
 
     @staticmethod
-    def display_output_for_analyzer(analyzer):
+    def display_output_for_analyzer(analyzer, output=sys.stdout):
         if analyzer.processed is False:
-            raise 'Analyzer must be processed before output display. You must call `analyze()` in your instance'
+            raise AnalyzerNotProcessedError('Analyzer must be processed before output display. You must call `analyze()` in your instance')
 
-        CLI.print_results(analyzer.results())
+        CLI.print_results(analyzer.results(), output=output)
 
     @staticmethod
-    def format_table_data(results):
+    def format_table_data(results, colorize=True):
         out_data = []
 
         format_color = lambda string, cname: ('\x1b[%sm%s \x1b[0m') % (CLI.COLORS[cname], string)
@@ -44,12 +48,15 @@ class CLI():
             # Get the current 'Part of Speech' name, such as: 'Adverb', 'Noun'
             pos_name = current_result.word.__class__.__name__
 
-            out_data.append([format_color(data[0], pos_name), format_color(pos_name, pos_name)])
+            out_data.append([
+                format_color(data[0], pos_name) if colorize else data[0],
+                format_color(pos_name, pos_name) if colorize else pos_name
+            ])
 
         return out_data
 
     @staticmethod
-    def print_results(results, width=15):
+    def print_results(results, width=15, output=sys.stdout):
         table_data = CLI.format_table_data(results)
 
-        return tableprint.table(table_data, CLI.OUTPUT_TABLE_HEADERS, width=width)
+        return tableprint.table(table_data, CLI.OUTPUT_TABLE_HEADERS, width=width, out=output)
