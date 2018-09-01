8b00583d633fd3cef379aa170adcfded9be82972
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-09-01 13:08:58 -0300

    Added some basic CLI module. Inside code: 'CLI.run(sentence)'

---
+++ esperanto_analyzer/cli/cli.py
@@ -0,0 +1,55 @@
+# pylint: disable=missing-docstring
+import tableprint
+
+from esperanto_analyzer import MorphologicalSentenceAnalyzer
+
+class CLI():
+    COLORS = {
+        'Adjective': 92,    # Light Green
+        'Adverb': 32,       # Green
+        'Article': 33,      # Yellow
+        'Conjunction': 35,  # Magenta
+        'Interjection': 95, # Light Magenta
+        'Noun': 34,         # Blue
+        'Numeral': 93,      # Light Yellow
+        'Preposition': 36,  # Cian
+        'Pronoun': 96,      # Light Cian
+        'Verb': 31          # Red
+    }
+
+    OUTPUT_TABLE_HEADERS = ['Word', 'Part of Speech']
+
+    @staticmethod
+    def run(input_sentence=None):
+        analyzer = MorphologicalSentenceAnalyzer(input_sentence)
+        analyzer.analyze()
+
+        CLI.display_output_for_analyzer(analyzer)
+
+    @staticmethod
+    def display_output_for_analyzer(analyzer):
+        if analyzer.processed is False:
+            raise 'Analyzer must be processed before output display. You must call `analyze()` in your instance'
+
+        CLI.print_results(analyzer.results())
+
+    @staticmethod
+    def format_table_data(results):
+        out_data = []
+
+        format_color = lambda string, cname: ('\x1b[%sm%s \x1b[0m') % (CLI.COLORS[cname], string)
+
+        for data in results:
+            current_result = data[1].result
+            # Get the current 'Part of Speech' name, such as: 'Adverb', 'Noun'
+            pos_name = current_result.word.__class__.__name__
+
+            out_data.append([format_color(data[0], pos_name), format_color(pos_name, pos_name)])
+
+        return out_data
+
+    @staticmethod
+    def print_results(results, width=15):
+        table_data = CLI.format_table_data(results)
+
+        return tableprint.table(table_data, CLI.OUTPUT_TABLE_HEADERS, width=width)
