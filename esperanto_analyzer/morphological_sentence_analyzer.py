627dc1c992144e41e4da5c98a71e46505c4265b9
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-09-01 10:43:32 -0300

    [100% TESTS COVERAGE :)] Basic tests added to MorphologicalSentenceAnalyzer

--- esperanto_analyzer/morphological_sentence_analyzer.py
+++ esperanto_analyzer/morphological_sentence_analyzer.py
@@ -11,24 +11,32 @@ class MorphologicalSentenceAnalyzer:
         self.sentence = sentence
         self.sentence_words = sentence.split()
         self.processed = False
-        self.results = []
+        self.results = None
 
-    def analyzes(self):
+    def analyze(self):
         # Avoid running the same thing many times returning the previous cached results
         if self.processed is True:
-            return self.results
+            return None
 
         # Cache the results
         self.results = self._process_words(self.sentence_words)
         self.processed = True
 
-        return self.results
+        return True
+
+    def analyzes_results(self):
+        if not self.processed:
+            return None
+
+        return [result.results for result in self.results]
 
     def _process_words(self, words):
         results = []
 
         for word in words:
-            processor = MorphologicalAnalyzer(word)
-            results.append(processor.process())
+            analyzer = MorphologicalAnalyzer(word)
+            analyzer.analyze()
+
+            results.append(analyzer)
 
         return results
